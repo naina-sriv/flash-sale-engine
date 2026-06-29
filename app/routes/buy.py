d010ad57bb192c458288b1b07e52d3dca4e46851
naina-sriv <146738328+naina-sriv@users.noreply.github.com>
2026-06-30 03:52:55 +0530

    feat: refactor click_buy function; improve item reservation logic and error messages

--- app/routes/buy.py
+++ app/routes/buy.py
@@ -4,26 +4,35 @@ from app.models.requests import BuyRequest
 from app.dependencies.auth import get_current_user
 
 router= APIRouter(prefix="/buy")
-
+flash_inventory=["1","3"]
 @router.post("/")
 def click_buy(req:BuyRequest, user_id:str=Depends(get_current_user)):
+    if redis_client.exists(f"purchased_flash:{user_id}"):
+        return {"message":"only one flash order per customer"}
+    lease_key=f"lease:{user_id}"
+    if redis_client.exists(lease_key):
+        return {"message":f"some item is already reserved for you."}
     if req.item_id==[]:
         return {"message":"no item in cart"}
     if len(req.item_id)!=len(set(req.item_id)):
         return {"message": "duplicates not allowed"}
+    count=0
+    for i in req.item_id:
+        if i in flash_inventory:
+            count+=1
+    if count>1:
+        return {"message":"You can buy only one flash sale item per order"}
     reserved_items=[]
     for i in req.item_id:
-        lease_key=f"lease:{user_id}:{i}"
-        if redis_client.exists(lease_key):
-            return {"message":f"the item {i} is already reserved for you."}
         new_val=redis_client.decr(f"stock:{i}")
         if new_val<0:
             for j in reserved_items:
                 redis_client.incr(f"stock:{j}")
-                redis_client.delete(f"lease:{user_id}:{j}")
+            redis_client.delete(f"lease:{user_id}")
             return {"message": f"out of stock item {i}"}
         else:
-            redis_client.setex(lease_key,300,"active")
             reserved_items.append(i)
-            continue
+    redis_client.setex(lease_key, 300, "active")
+    if count==1:
+        redis_client.setex(f"purchased_flash:{user_id}",89400,"active")
     return {"message": "success"} 
